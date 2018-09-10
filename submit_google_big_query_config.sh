bd5c773135d034b8e493e13a9d4ed4480ed79584
Chris Matta <chris@confluent.io>
2018-09-10 19:34:15 -0400

    forgot to add the key converter to the big query connector config

--- submit_google_big_query_config.sh
+++ submit_google_big_query_config.sh
@@ -18,6 +18,7 @@ DATA=$( cat << EOF
     "autoUpdateSchemas": true,
     "schemaRetriever": "com.wepay.kafka.connect.bigquery.schemaregistry.schemaretriever.SchemaRegistrySchemaRetriever",
     "schemaRegistryLocation": "http://35.231.187.35:8081,http://35.237.49.31:8081",
+    "key.converter":"org.apache.kafka.connect.storage.StringConverter",
     "bufferSize": 100000,
     "maxWriteSize": 10000,
     "tableWriteWait": 1000,
